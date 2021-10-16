2a41c96964e32bb0bd4ad9868c9d24182df93ff1
Carla Melo Reis <cmelorei@everis.com>
2021-10-16 15:10:02 -0300

    Adicionando um segundo caso de testes

--- src/main/java/com/alura/appium/App.java
+++ src/main/java/com/alura/appium/App.java
@@ -13,16 +13,16 @@ public class App {
 
     public static void main( String[] args ) throws MalformedURLException {
 
-        File apk = new File("C:\\Users\\cmelorei\\OneDrive - everis\\Documentos\\Alura\\projeto-appium\\src\\main\\resources\\alura_esporte.apk");
-
-        DesiredCapabilities configuracoes = new DesiredCapabilities();
-        configuracoes.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
-        configuracoes.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
-        configuracoes.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-
-        URL urlConexao = new URL("http://127.0.0.1:4723/wd/hub");
-
-        AppiumDriver driver = new AppiumDriver<>(urlConexao, configuracoes);
+//        File apk = new File("C:\\Users\\cmelorei\\OneDrive - everis\\Documentos\\Alura\\projeto-appium\\src\\main\\resources\\alura_esporte.apk");
+//
+//        DesiredCapabilities configuracoes = new DesiredCapabilities();
+//        configuracoes.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
+//        configuracoes.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+//        configuracoes.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+//
+//        URL urlConexao = new URL("http://127.0.0.1:4723/wd/hub");
+//
+//        AppiumDriver driver = new AppiumDriver<>(urlConexao, configuracoes);
 
     }
 }
